eee77be9f4ce19b20b71fe3105368bc04ee801c6
Shivang <shivang@pop-os.localdomain>
2023-02-18 12:50:33 +0530

    refectoring

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -7,45 +7,45 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-map("n", "<leader>,", ":cd ~/.config/nvim/lua<CR>")
-map("n", "<leader>i", "i <Esc>r")
-map("n", "<leader>I", "I <Esc>r")
-map("n", "<leader>a", "a <Esc>r")
-map("n", "<leader>A", "A <Esc>r")
+map("n", "<leader>,", "<cmd> cd ~/.config/nvim/lua<CR>")
+map("n", "<leader>i", "i r")
+map("n", "<leader>I", "I r")
+map("n", "<leader>a", "a r")
+map("n", "<leader>A", "A r")
 map("n", "<leader>p", "\"_dP")
-map("n", ";;", "A;<Esc>-")
-map("n","<leader>fx",":!chmod +x %<CR>")
+map("n", ";;", "A;-")
+map("n","<leader>fx","<cmd> !chmod +x %<CR>")
 
 -- Telescope shortcuts
-map("n", "<leader>cd", ":Telescope cder<CR>")
-map("n", "<leader>j", ":Telescope aerial<CR>")
-map("n", "<leader>fr", ":Telescope frecency<CR>")
+map("n", "<leader>cd", "<cmd> Telescope cder<CR>")
+map("n", "<leader>j", "<cmd> Telescope aerial<CR>")
+map("n", "<leader>fr", "<cmd> Telescope frecency<CR>")
 
 -- Git shortcuts
-map("n", "<leader>ga", ":G add %<CR>")
-map("n", "<leader>gaa", ":G add -u<CR>")
-map("n", "<leader>gA", ":G add .<CR>")
-map("n", "<leader>gc", ":G commit ")
-map("n", "<leader>G", ":G ")
-map("n", "<leader>g", ":Neogit<CR>")
+map("n", "<leader>ga", "<cmd> G add %<CR>")
+map("n", "<leader>gaa", "<cmd> G add -u<CR>")
+map("n", "<leader>gA", "<cmd> G add .<CR>")
+map("n", "<leader>gc", "<cmd> G commit ")
+map("n", "<leader>G", "<cmd> G ")
+map("n", "<leader>g", "<cmd> Neogit<CR>")
 
 -- Trouble.nvim
-map("n","<leader>tx ",":TroubleToggle<CR>")
-map("n","<leader>tw ",":TroubleToggle workspace_diagnostics<CR>")
-map("n","<leader>td ",":TroubleToggle document_diagnostics<CR>")
-map("n","<leader>tq ",":TroubleToggle quickfix<CR>")
-map("n","<leader>tl ",":TroubleToggle loclist<CR>")
-map("n","gr",":TroubleToggle lsp_references<CR>")
+map("n","<leader>tx ","<cmd> TroubleToggle<CR>")
+map("n","<leader>tw ","<cmd> TroubleToggle workspace_diagnostics<CR>")
+map("n","<leader>td ","<cmd> TroubleToggle document_diagnostics<CR>")
+map("n","<leader>tq ","<cmd> TroubleToggle quickfix<CR>")
+map("n","<leader>tl ","<cmd> TroubleToggle loclist<CR>")
+map("n","gr","<cmd> TroubleToggle lsp_references<CR>")
 
 -- Symbols def
-map("n","<leader>d",":SymbolsOutline<CR>")
+map("n","<leader>d","<cmd> SymbolsOutline<CR>")
 
 -- Refactor.nvim
 -- Remaps for the refactoring operations currently offered by the plugin
-vim.api.nvim_set_keymap("v", "<leader>re", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR>]], {noremap = true, silent = true, expr = false})
-vim.api.nvim_set_keymap("v", "<leader>rf", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>]], {noremap = true, silent = true, expr = false})
-vim.api.nvim_set_keymap("v", "<leader>rv", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]], {noremap = true, silent = true, expr = false})
-vim.api.nvim_set_keymap("v", "<leader>ri", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
+vim.api.nvim_set_keymap("v", "<leader>re", [[ <Cmd>lua require('refactoring').refactor('Extract Function')<CR>]], {noremap = true, silent = true, expr = false})
+vim.api.nvim_set_keymap("v", "<leader>rf", [[ <Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>]], {noremap = true, silent = true, expr = false})
+vim.api.nvim_set_keymap("v", "<leader>rv", [[ <Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]], {noremap = true, silent = true, expr = false})
+vim.api.nvim_set_keymap("v", "<leader>ri", [[ <Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
 
 -- Extract block doesn't need visual mode
 vim.api.nvim_set_keymap("n", "<leader>rb", [[ <Cmd>lua require('refactoring').refactor('Extract Block')<CR>]], {noremap = true, silent = true, expr = false})
@@ -53,4 +53,4 @@ vim.api.nvim_set_keymap("n", "<leader>rbf", [[ <Cmd>lua require('refactoring').r
 
 -- Inline variable can also pick up the identifier currently under the cursor without visual mode
 vim.api.nvim_set_keymap("n", "<leader>ri", [[ <Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
-vim.api.nvim_set_keymap("v","<leader>rr","<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",	{ noremap = true })
+vim.api.nvim_set_keymap("v","<leader>rr","<cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",	{ noremap = true })
